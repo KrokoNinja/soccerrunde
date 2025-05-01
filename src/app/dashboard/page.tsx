0b31e075484a02820ab675d5a290ec0f11aac052
KrokoNinja <79910460+KrokoNinja@users.noreply.github.com>
2025-05-01 18:45:39 +0200

    fix: build errors

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,11 +1,13 @@
 "use client"
 import { useQuery } from "convex/react"
 import { api } from "../../../convex/_generated/api";
+import CreateGameday from "@/components/CreateGameday";
 export default function Dashboard() {
 
   const field_price = useQuery(api.info.get_field_price);
   
   return <div>
     <div>Dashboard: {field_price}</div>
+    <CreateGameday />
   </div>
 }
--- src/components/AgGrid.tsx
+++ src/components/AgGrid.tsx
@@ -9,18 +9,23 @@ import { api } from '@/../convex/_generated/api';
 import { useQuery } from 'convex/react';
 
 export default function AgGrid() {
-  const [rowData, setRowData] = useState<{ name: string; balance: number }[]>([]);
+  const [rowData, setRowData] = useState<{ name: string; balance: string }[]>([]);
   const players = useQuery(api.player.get_players);
+
+
   useEffect(() => {
     if (players) {
-      setRowData(players);
+      setRowData(players.map((player) => ({
+        name: player.name,
+        balance: player.balance + "€",
+      })));
     }
   }, [players]);
 
-  const [colDefs, setColDefs] = useState<ColDef<any>[]>([
-      { field: "name" },
-      { field: "balance" },
-  ]);
+  const colDefs: ColDef<{ name: string; balance: string }>[] = [
+      { headerName: "Name", field: "name" },
+      { headerName: "Kontostand", field: "balance" },
+  ];
 
   return (
     <div style={{ height: 500, width: "100%" }}>
